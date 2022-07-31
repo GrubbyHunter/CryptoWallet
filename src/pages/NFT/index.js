dde815904a9bf8e91bdffdab5bfd69c511c86854
GrubbyHunter <GrubbyHunter@gmail.com>
2022-07-31 17:03:03 +0800

    feature add:push name

--- src/pages/NFT/index.js
+++ src/pages/NFT/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text, View } from 'react-native'
-
+// import
 const NFTPage = () => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -8,5 +8,9 @@ const NFTPage = () => {
     </View>
   );
 }
-
+ImagePicker.openPicker({
+  multiple: true
+}).then(images => {
+  console.log(images);
+});
 export default NFTPage
